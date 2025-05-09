ac98f6cf7a26500c0336e0369682e6eca58ebb92
BLaTNoY98 <dilshod.uz.98.01@gmail.com>
2025-05-09 14:44:43 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-# Add your Telegram bot token and settings here
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Environment Variables
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
