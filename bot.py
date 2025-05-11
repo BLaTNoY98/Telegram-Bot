da920bdf9314b8361eedb078f94779cac9ce7164
BLaTNoY98 <dilshod.uz.98.01@gmail.com>
2025-05-11 23:50:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -184,8 +184,13 @@ if __name__ == "__main__":
     application.add_handler(MessageHandler(filters.CONTACT, handle_contact))
     application.add_handler(CallbackQueryHandler(callback_handler))
 
-    application.add_handlers(get_operator_panel_handlers())
-    application.add_handlers(get_targetolog_panel_handlers())
-    application.add_handlers(get_admin_handlers())
+  for handler in get_operator_panel_handlers():
+    application.add_handler(handler)
+
+for handler in get_targetolog_panel_handlers():
+    application.add_handler(handler)
+
+for handler in get_admin_handlers():
+    application.add_handler(handler)
 
     application.run_polling()
