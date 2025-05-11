8bd83a7875c8e66c8763fab8076d6dcbd9a48a98
BLaTNoY98 <dilshod.uz.98.01@gmail.com>
2025-05-12 01:28:30 +0500

    Update delete_webhook.py

--- delete_webhook.py
+++ delete_webhook.py
@@ -2,8 +2,13 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # .env fayldan o'zgaruvchilarni o'qish
+
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+if not BOT_TOKEN:
+    print("❌ TOKEN topilmadi. Iltimos, .env faylga TELEGRAM_TOKEN qo‘shing.")
+    exit()
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook"
 response = requests.get(url)
 print("Webhook delete response:", response.json())
